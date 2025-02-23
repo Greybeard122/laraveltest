f1af828ca132d23121a90725a9a8db6db3c85087
Darius <dariusanity122@gmail.com>
2025-02-23 23:55:08 +0800

    fix route middleware again

--- app/Http/Middleware/StudentAuthMiddleware.php
+++ app/Http/Middleware/StudentAuthMiddleware.php
@@ -5,20 +5,13 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Log;
 
 class StudentAuthMiddleware
 {
     public function handle(Request $request, Closure $next)
     {
-        Log::info('Middleware Check: Student Authenticated?', [
-            'authenticated' => Auth::guard('student')->check(),
-            'user' => Auth::guard('student')->user()
-        ]);
-
         if (!Auth::guard('student')->check()) {
-            Log::warning('Middleware Redirect: Student Not Authenticated');
-            return redirect()->route('login')->with('error', 'Please log in first.');
+            return redirect()->route('login');
         }
 
         return $next($request);
