a9315218d6861244876f4aabac0cb679aadd43a9
Darius <dariusanity122@gmail.com>
2025-02-27 21:14:51 +0800

    http for railway

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,9 +19,13 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot()
 {
-    if (env('APP_ENV') === 'production') { // Only enforce HTTPS in production
-        URL::forceScheme('https');
-    }
+    /* if (env('APP_ENV') === 'production') { // Only enforce HTTPS in production
+        URL::forceScheme('https'); */
+        if (env('APP_ENV') === 'production') { // Force HTTPS in production
+            URL::forceScheme('https');
+        }
+
+    
 
     Log::info('Application boot completed');
     Paginator::useTailwind();
