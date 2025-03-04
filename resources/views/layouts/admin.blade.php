7bc8f0d73a2765a48284fe972a8d34763cc19090
Darius <dariusanity122@gmail.com>
2025-03-04 17:13:58 +0800

    side nav icons

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -9,6 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="{{ asset('css/responsive-admin.css') }}" rel="stylesheet">
     <link rel="icon" href="{{ asset('images/PRMSU.png') }}" type="image/x-icon">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 <body>
     <x-messages/>
